fe1f88b947f349d97bdcac90ae3a0b1c51f432c3
内田ありさ <ukihima.3250@gmail.com>
2024-07-16 23:14:03 +0900

    会員登録デザイン

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -6,44 +6,44 @@
 
 @section('content')
 <div class="register__container">
-    <div class="register-form__heading">
-        <h2>Registration</h2>
+    <div class="register__inner">
+        <div class="register-form__heading">
+            <h2>Registration</h2>
+        </div>
+        <form class="register-form" action="/register" method="post">
+            @csrf
+            <div class="form__group">
+                <div class="form__input-name">
+                    <input class="form__input-text" type="text" name="name" placeholder="Username" value="{{ old('name') }}" />
+                </div>
+                <div class="form__error">
+                    @error('name')
+                    {{ $message }}
+                    @enderror
+                </div>
+            </div>
+            <div class="form__group">
+                <div class="form__input-mail">
+                    <input class="form__input-text" type="email" name="email" placeholder="Email" value="{{ old('email') }}" />
+                </div>
+                <div class="form__error">
+                    @error('email')
+                    {{ $message }}
+                    @enderror
+                </div>
+            </div>
+            <div class="form__group">
+                <div class="form__input-password">
+                    <input class="form__input-text" type="password" placeholder="Password" name="password" />
+                </div>
+                <div class="form__error">
+                    @error('password')
+                    {{ $message }}
+                    @enderror
+                </div>
+            </div>
+            <input class="register-form__btn btn" type="submit" value="登録">
+        </form>
     </div>
-    <form class="register-form" action="/register" method="post">
-        @csrf
-        <div class="form__group">
-            <div class="form__input-name">
-                <input class="form__input-text" type="text" name="name" placeholder="Username" value="{{ old('name') }}" />
-            </div>
-            <div class="form__error">
-                @error('name')
-                {{ $message }}
-                @enderror
-            </div>
-        </div>
-        <div class="form__group">
-            <div class="form__input-mail">
-                <input class="form__input-text" type="email" name="email" placeholder="Email" value="{{ old('email') }}" />
-            </div>
-            <div class="form__error">
-                @error('email')
-                {{ $message }}
-                @enderror
-            </div>
-        </div>
-        <div class="form__group">
-            <div class="form__input-password">
-                <input class="form__input-text" type="password" placeholder="Password" name="password" />
-            </div>
-            <div class="form__error">
-                @error('password')
-                {{ $message }}
-                @enderror
-            </div>
-        </div>
-        <div class="form__button">
-            <button class="form__button-submit" type="submit">登録</button>
-        </div>
-    </form>
 </div>
 @endsection
