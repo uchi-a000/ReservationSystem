a8426a312e0958da3901e3e17bb626912df304e2
内田ありさ <ukihima.3250@gmail.com>
2024-09-21 15:15:35 +0900

    コード修正

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -20,28 +20,27 @@ class DatabaseSeeder extends Seeder
         $this->call(ShopsTableSeeder::class);
 
         //管理者
-        $adminRole = Role::create(['name' => 'admin']);
+        $admin_role = Role::create(['name' => 'admin']);
         //店舗代表者
-        $shopRepRole = Role::create(['name' => 'shop_representative']);
+        $shop_rep_role = Role::create(['name' => 'shop_representative']);
 
         //権限
-        $manageUserPermission = Permission::create(['name' => 'manage users']);
-        $manageShopsPermission = Permission::create(['name' => 'manage shops']);
-        $viewReservationPermission = Permission::create(['name' => 'view reservations']);
+        $manage_user_permission = Permission::create(['name' => 'manage users']);
+        $manage_shops_permission = Permission::create(['name' => 'manage shops']);
+        $view_reservation_permission = Permission::create(['name' => 'view reservations']);
 
         //役割に権限を付与
-        $adminRole->givePermissionTo($manageUserPermission, $manageShopsPermission);
+        $admin_role->givePermissionTo($manage_user_permission, $manage_shops_permission);
 
-        $shopRepRole->givePermissionTo($manageShopsPermission, $viewReservationPermission);
+        $shop_rep_role->givePermissionTo($manage_shops_permission, $view_reservation_permission);
 
-
-        $adminUser = User::create([
+        $admin_user = User::create([
             'name' => '管理者',
             'email' => 'admin@example.com',
             'password' => bcrypt('pppp0000'),
             'email_verified_at' => now()
         ]);
 
-        $adminUser->assignRole('admin');
+        $admin_user->assignRole('admin');
     }
 }
